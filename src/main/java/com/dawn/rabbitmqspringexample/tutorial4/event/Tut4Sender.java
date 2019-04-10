93bcab676ed0efdb95b2bdea2c6a16420abc9023
Shahar Zror <sha4321@gmail.com>
2019-04-10 10:25:24 +0300

    Tutorial 4

---
+++ src/main/java/com/dawn/rabbitmqspringexample/tutorial4/event/Tut4Sender.java
@@ -0,0 +1,40 @@
+package com.dawn.rabbitmqspringexample.tutorial4.event;
+
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.core.FanoutExchange;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.IntStream;
+
+public class Tut4Sender {
+
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+
+    @Autowired
+    private DirectExchange direct;
+
+    AtomicInteger index = new AtomicInteger(0);
+
+    AtomicInteger count = new AtomicInteger(0);
+
+    private final String[] keys = {"orange", "black", "green"};
+
+    @Scheduled(fixedDelay = 1000, initialDelay = 500)
+    public void send(){
+
+        StringBuilder builder = new StringBuilder("Hello to ");
+        if (this.index.incrementAndGet() == 3) {
+            this.index.set(0);
+        }
+        String key = keys[this.index.get()];
+        builder.append(key).append(' ');
+        builder.append(this.count.getAndIncrement());
+        String message = builder.toString();
+        rabbitTemplate.convertAndSend(direct.getName(), key, message);
+        System.out.println(" [x] Sent '" + message + "'");
+    }
+}
