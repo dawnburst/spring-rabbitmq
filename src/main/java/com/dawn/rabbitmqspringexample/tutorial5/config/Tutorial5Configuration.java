40da6560434f4198b594f996a2cb4965bdcb589f
Shahar Zror <sha4321@gmail.com>
2019-04-10 10:44:27 +0300

    Tutorial 5

---
+++ src/main/java/com/dawn/rabbitmqspringexample/tutorial5/config/Tutorial5Configuration.java
@@ -0,0 +1,68 @@
+package com.dawn.rabbitmqspringexample.tutorial5.config;
+
+import com.dawn.rabbitmqspringexample.tutorial5.event.Tut5Receiver;
+import com.dawn.rabbitmqspringexample.tutorial5.event.Tut5Sender;
+import org.springframework.amqp.core.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Profile({"tut5","topics"})
+@Configuration
+public class Tutorial5Configuration {
+
+    @Bean
+    public TopicExchange topic() {
+        return new TopicExchange("tut.topic");
+    }
+
+    @Profile("receiver")
+    private static class ReceiverConfig {
+
+        @Bean
+        public Tut5Receiver receiver() {
+            return new Tut5Receiver();
+        }
+
+        @Bean
+        public Queue autoDeleteQueue1() {
+            return new AnonymousQueue();
+        }
+
+        @Bean
+        public Queue autoDeleteQueue2() {
+            return new AnonymousQueue();
+        }
+
+        @Bean
+        public Binding binding1a(TopicExchange topic,
+                                 Queue autoDeleteQueue1) {
+            return BindingBuilder.bind(autoDeleteQueue1)
+                    .to(topic)
+                    .with("*.orange.*");
+        }
+
+        @Bean
+        public Binding binding1b(TopicExchange topic,
+                                 Queue autoDeleteQueue1) {
+            return BindingBuilder.bind(autoDeleteQueue1)
+                    .to(topic)
+                    .with("*.*.rabbit");
+        }
+
+        @Bean
+        public Binding binding2a(TopicExchange topic,
+                                 Queue autoDeleteQueue2) {
+            return BindingBuilder.bind(autoDeleteQueue2)
+                    .to(topic)
+                    .with("lazy.#");
+        }
+
+    }
+
+    @Profile("sender")
+    @Bean
+    public Tut5Sender sender() {
+        return new Tut5Sender();
+    }
+}
